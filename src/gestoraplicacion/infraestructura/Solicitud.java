9f2f770fc515b0b3829a7d50ee4a491c6eb9906b
Richard Montoya <richard.a.montoya.l@gmail.com>
2020-09-25 19:55:36 -0500

    Añadir variables definidas en el uml para Room, Solicitud y Persona

--- src/gestoraplicacion/infraestructura/Solicitud.java
+++ src/gestoraplicacion/infraestructura/Solicitud.java
@@ -1,5 +1,37 @@
 package gestoraplicacion.infraestructura;
 
-public class Solicitud {
+import gestoraplicacion.usuarios.Persona;
 
+public class Solicitud {
+	private String codigo;
+	private Persona solicitante;
+	private Procedimiento solicitud;
+	private boolean aprobado;
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public Persona getSolicitante() {
+		return solicitante;
+	}
+	public void setSolicitante(Persona solicitante) {
+		this.solicitante = solicitante;
+	}
+	public Procedimiento getSolicitud() {
+		return solicitud;
+	}
+	public void setSolicitud(Procedimiento solicitud) {
+		this.solicitud = solicitud;
+	}
+	public boolean isAprobado() {
+		return aprobado;
+	}
+	public void setAprobado(boolean aprobado) {
+		this.aprobado = aprobado;
+	}
+	
+	
 }
--- src/gestoraplicacion/usuarios/Persona.java
+++ src/gestoraplicacion/usuarios/Persona.java
@@ -1,5 +1,43 @@
 package gestoraplicacion.usuarios;
 
-public class Persona {
+import java.util.Date;
 
+public class Persona {
+	private String nombre;
+	private String id;
+	private Date fechaNacimiento;
+	private String telefono;
+	private String direccion;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	
 }
