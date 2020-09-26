06c908f17e52a9c861198ddc0b4950c0dc9fb4a5
IThiagoRo <isrojasm@unal.edu.co>
2020-09-25 20:36:08 -0500

    Creando y Modificando las clases Medico y Hospital

--- src/gestoraplicacion/infraestructura/Hospital.java
+++ src/gestoraplicacion/infraestructura/Hospital.java
@@ -1,5 +1,66 @@
 package gestoraplicacion.infraestructura;
 
 public class Hospital {
+	private String nit;
+	private String nombre;
+	private int camasDisponibles;
+	private Administrador administrador;
+	private Paciente[] pacientes;
+	private Medico[] medicos;
+	private Room[] habitaciones;
 
+	public String getNit(){
+		return nit;
+	}
+
+	public void setNit(String nit){
+		this.nit = nit;
+	}
+
+	public String getNombre(){
+		return nombre;
+	}
+
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+
+	public int getCamasDisponibles(){
+		return camasDisponibles;
+	}
+
+	public void setCamasDisponibles(int camasDisponibles){
+		this.camasDisponibles = camasDisponibles
+	}
+	
+	public Administrador getAdministrador(){
+		return administrador;
+	}
+
+	public void setAdministrador(Administrador administrador){
+		this.administrador = administrador;
+	}
+
+	public Paciente[] getPacientes(){
+		return pacientes;
+	}
+
+	public void setPacientes(Paciente[] pacientes){
+		this.pacientes = pacientes;
+	}
+
+	public Medico[] getMedicos(){
+		return medicos;
+	}
+
+	public void setMedicos(Medico[] medicos){
+		this.medicos = medicos;
+	}
+
+	public Room[] getHabitaciones(){
+		return habitaciones;
+	}
+	public void setHabitaciones(Room[] habitaciones){
+		this.habitaciones = habitaciones;
+	}
 }
--- src/gestoraplicacion/usuarios/Medico.java
+++ src/gestoraplicacion/usuarios/Medico.java
@@ -1,5 +1,64 @@
 package gestoraplicacion.usuarios;
 
-public class Medico {
+public class Medico extends Persona{
+	private int especialidad;
+	private boolean[] horario;
+	private Procedimiento[] procedAsignados;
+	private Hospital hospital;
+	private Solicitud[] estadoSolicitudes;
+	private int estadoCuenta;
+	
+	
+	public Medico() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getEspecialidad() {
+		return especialidad;
+	}
 
+	public void setEspecial(int especialidad) {
+		this.especialidad = especialidad;
+	}
+
+	public boolean[] getHorario() {
+		return horario;
+	}
+
+	public void setHorario(boolean[] horario) {
+		this.horario = horario;
+	}
+
+	public Procedimiento[] getProcedAsignados() {
+		return procedAsignadas;
+	}
+	
+	public void setProcedAsignados(Procedimiento[] procedAsignadas) {
+		this.procedAsignadas = procedAsignadas;
+	}
+
+	public Hospital getHospital() {
+		return hospital;
+	}
+
+	public void setHospital(Hospital hospital) {
+		this.hospital = hospital;
+	}
+
+	public Solicitud[] getEstadoSolicitudes() {
+		return estadoSolicitudes;
+	}
+
+	public void setEstadoSolicitudes(Solicitud[] estadoSolicitudes) {
+		this.estadoSolicitues = estadoSolicitudes;
+	}
+
+	public int getEstadoCuenta() {
+		return estadoCuenta;
+	}
+
+	public void setEstadoCuenta(int estadoCuenta) {
+		this.estadoCuenta = estadoCuenta;
+	}
 }
