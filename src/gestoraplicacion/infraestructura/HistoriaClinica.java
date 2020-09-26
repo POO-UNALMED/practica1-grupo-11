4238222d6b3c36134e944108c300f93bd2221803
Luis Santiago <55595208+SantiagoMaya99@users.noreply.github.com>
2020-09-25 20:04:26 -0500

    atributos, getters y setters y constructores de las clases Administrador e HistoriaClinica

--- src/gestoraplicacion/infraestructura/HistoriaClinica.java
+++ src/gestoraplicacion/infraestructura/HistoriaClinica.java
@@ -1,5 +1,45 @@
 package gestoraplicacion.infraestructura;
+import gestoraplicacion.usuarios.Paciente;
+
 
 public class HistoriaClinica {
+	
+	private String codigo;
+	private Paciente paciente;
+	private Procedimiento[] procedimientos;
+	
+	public HistoriaClinica() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
+	
+	public HistoriaClinica(String codigo, Paciente paciente, Procedimiento[] procedimientos) {
+		super();
+		this.codigo = codigo;
+		this.paciente = paciente;
+		this.procedimientos = procedimientos;
+	}
+	
+
+
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public Paciente getPaciente() {
+		return paciente;
+	}
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+	public Procedimiento[] getProcedimientos() {
+		return procedimientos;
+	}
+	public void setProcedimientos(Procedimiento[] procedimientos) {
+		this.procedimientos = procedimientos;
+	}
+	
 }
